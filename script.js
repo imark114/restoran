df68794d49168d3181a03b2aa72b8df72a174d4d
rakib114 <mahmud17209@gmail.com>
2024-06-03 19:35:39 +0600

    building a simple restauran management system design using tailwindcss

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggoler = document.getElementById("togoller")
+const nav_bar = document.getElementById("nav_bar")
+toggoler.addEventListener("click",()=>{
+    nav_bar.classList.toggle("hidden")
+})
